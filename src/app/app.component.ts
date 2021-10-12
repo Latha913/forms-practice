d7646ff98a748be72e5198d6d3619a135a019807
unknown <lathakoduri994@gmail.com>
2021-10-12 19:26:53 -0400

    setting and patching form values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,20 @@ export class AppComponent {
 
   defaultQuestion = "pet"
   suggestUserName() {
-    const suggestedName = 'Superuser';
+ const suggestedName = 'Superuser';
+//  this.signupForm.setValue({
+//    userData:{
+//     username: suggestedName,
+//     email: ""
+//    },
+//    secret:"pet",
+//    questionAnswer:" ",
+//    gender: "male"
+//  })
+this.signupForm.form.patchValue({
+  userData:{username: suggestedName,
+  }
+})
   }
 
 //   onSubmit(form: NgForm){
